140633bbb1c2da38385cfeff6cabc12834bb0533
Lee Sanderson <lee.sndrsn@gmail.com>
2024-05-11 12:03:07 +0100

    Refactor Directory info extenstions for clarity

--- BlogToHtml/DirectoryInfoExtensions.cs
+++ BlogToHtml/DirectoryInfoExtensions.cs
@@ -9,38 +9,6 @@ namespace BlogToHtml
 
     internal static class DirectoryInfoExtensions
     {
-        public static IDirectoryInfo ToDirectoryInfo(this IFileSystem fileSystem, string? path, bool createIfNotExists = false)
-        {
-            if (path == null) throw new ArgumentNullException(nameof(path));
-
-            var result = fileSystem.DirectoryInfo.New(path);
-            if (!result.Exists && createIfNotExists)
-            {
-                result.Create();
-                result = fileSystem.DirectoryInfo.New(path);
-            }
-
-            if (!result.Exists)
-            {
-                throw new Exception($"Directory {path} does not exist");
-            }
-
-            return result;
-        }
-
-        public static IFileInfo ToFileInfo(this IFileSystem fileSystem, string? path, bool errorIfNotExists = true)
-        {
-            if (path == null) throw new ArgumentNullException(nameof(path));
-
-            var result = fileSystem.FileInfo.New(path);
-            if (errorIfNotExists && !result.Exists)
-            {
-                throw new Exception($"File {path} does not exist");
-            }
-
-            return result;
-        }
-
         public static IDirectoryInfo CreateIfNotExists(this IDirectoryInfo directoryInfo)
         {
             if (!directoryInfo.Exists)
---
+++ BlogToHtml/FileSystemExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.IO.Abstractions;
+using System;
+
+namespace BlogToHtml
+{
+    internal static class FileSystemExtensions
+    {
+        public static IDirectoryInfo ToDirectoryInfo(this IFileSystem fileSystem, string? path, bool createIfNotExists = false)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+
+            var result = fileSystem.DirectoryInfo.New(path);
+            if (!result.Exists && createIfNotExists)
+            {
+                result.Create();
+                result = fileSystem.DirectoryInfo.New(path);
+            }
+
+            if (!result.Exists)
+            {
+                throw new Exception($"Directory {path} does not exist");
+            }
+
+            return result;
+        }
+
+        public static IFileInfo ToFileInfo(this IFileSystem fileSystem, string? path, bool errorIfNotExists = true)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+
+            var result = fileSystem.FileInfo.New(path);
+            if (errorIfNotExists && !result.Exists)
+            {
+                throw new Exception($"File {path} does not exist");
+            }
+
+            return result;
+        }
+    }
+}
